4ea4725851d9df6f397719f021d78af6847bbf85
Myweb8520 <myweb8520@gmail.com>
2023-07-13 07:36:13 +0530

    Cmd

--- src/Crud/Crud.js
+++ src/Crud/Crud.js
@@ -69,6 +69,17 @@ const Crud = () => {
       upDateArray.splice(index,1);
       return upDateArray;
     })
+    setInputData({
+      fname: '',
+      lname: '',
+      email: '',
+      number: '',
+      gender: '',
+      liveindia: '',
+      address: '',
+      files: [],
+      agree: false,
+    })
   }
   console.log(inputdata.agree);
   useEffect(() => {
@@ -78,11 +89,11 @@ const Crud = () => {
     <section className="form_input">
       <div className="part_1">
         <form className="input_set" onSubmit={handleSubmit}>
-          <input type="text" placeholder="First Name" name="fname" value={inputdata.fname} onChange={handleChange} />
-          <input type="text" placeholder="Last Name" name="lname" value={inputdata.lname} onChange={handleChange} />
-          <input type="email" placeholder="Email" name="email" value={inputdata.email} onChange={handleChange} />
-          <input type="number" placeholder="Number" name="number" value={inputdata.number} onChange={handleChange} />
-          <select name="gender" value={inputdata.gender} onChange={handleChange}>
+          <input type="text" placeholder="First Name" name="fname" value={inputdata.fname} onChange={handleChange} required />
+          <input type="text" placeholder="Last Name" name="lname" value={inputdata.lname} onChange={handleChange} required />
+          <input type="email" placeholder="Email" name="email" value={inputdata.email} onChange={handleChange} required />
+          <input type="number" placeholder="Number" name="number" value={inputdata.number} onChange={handleChange} required />
+          <select name="gender" value={inputdata.gender} onChange={handleChange} required>
             <option value="" defaultValue hidden>Select</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -91,18 +102,18 @@ const Crud = () => {
           <div className="yes">
             <p> Are your live in India ?</p>
             <label className="radio_set" htmlFor="yes">
-              <input type="radio" id="yes" name="liveindia" value="Yes" checked={inputdata.liveindia == "Yes"} onChange={handleChange} />
+              <input type="radio" id="yes" name="liveindia" value="Yes" checked={inputdata.liveindia == "Yes"} onChange={handleChange} required />
               Yes
             </label>
             <label className="radio_set" htmlFor="no">
-              <input type="radio" id="no" name="liveindia" value="No" checked={inputdata.liveindia == "No"} onChange={handleChange} />
+              <input type="radio" id="no" name="liveindia" value="No" checked={inputdata.liveindia == "No"} onChange={handleChange} required />
               No
             </label>
           </div>
-          <input type="file" multiple onChange={handelFile} />
-          <textarea name="address" placeholder="Address" value={inputdata.address} onChange={handleChange}></textarea>
+          <input type="file" multiple onChange={handelFile} required />
+          <textarea name="address" placeholder="Address" value={inputdata.address} onChange={handleChange} required></textarea>
           <div className="chackebox_set">
-            <input type="checkbox" name="agree" checked={inputdata.agree} onChange={handleChange} id="agree" />
+            <input type="checkbox" name="agree" checked={inputdata.agree} onChange={handleChange} id="agree"/>
             <label htmlFor="agree">Accept Term & Agrement</label>
           </div>
           <button>Submit</button>
